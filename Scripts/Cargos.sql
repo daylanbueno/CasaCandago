8b4fda5720c34ede0a2d12cef67a33d33c5b3907
Dailan Bueno <daylansantos@gmail.com>
2018-01-01 20:05:21 -0200

    Atualização sql cargo.

--- Scripts/Cargos.sql
+++ Scripts/Cargos.sql
@@ -5,5 +5,4 @@ INSERT INTO `casacandango`.`cargo` (`nome`, `salario`) VALUES ('Administrador(a)
 INSERT INTO `casacandango`.`cargo` (`nome`, `salario`) VALUES ('Cuidador(a)', '1000.00');
 INSERT INTO `casacandango`.`cargo` (`nome`, `salario`) VALUES ('Mostorista', '1000.00');
 INSERT INTO `casacandango`.`cargo` (`nome`, `salario`) VALUES ('Emfemeira(a)', '1000.00');
-
 INSERT INTO `casacandango`.`cargo` (`nome`, `salario`) VALUES ('Psicólogo(a)', '1000.00');
